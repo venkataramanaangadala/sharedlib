bf453e37cc829280a37052a7dfdaf4d54f1e9f07
venkataramanaangadala <155218269+venkataramanaangadala@users.noreply.github.com>
2024-01-27 16:45:17 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -19,6 +19,14 @@ def executeSelenium(jobname)
  sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar" 
 }
 
+def gitDelivery(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+
+}
+
+
+
 
 
 
