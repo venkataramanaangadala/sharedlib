041ea5d24e1cf6d443ff81728ae73a9e2adb00e0
venkataramanaangadala <155218269+venkataramanaangadala@users.noreply.github.com>
2024-01-27 15:56:16 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -7,3 +7,11 @@ def gitBuild()
 {
    sh 'mvn package'
 }
+
+def gitDeployment(jobname,ip,context)
+{
+  sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${context}.war"
+
+}
+
+
